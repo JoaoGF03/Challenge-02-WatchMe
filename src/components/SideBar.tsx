6e9493fe121a2280a58c4eb0f87e33b0977183d1
JoaoGF03 <joao.g.f@icloud.com>
2021-08-24 19:37:50 -0300

    🚀 Challenge Completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
+import { Button } from './Button'
+
+interface SideBarProps {
+  selectedGenreId: number,
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>,
+  handleClickButton(id: number): void
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  const { genres, selectedGenreId, handleClickButton } = props
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
